f7161dd592443bd1df675334ba55db7ad08e47a9
Eduardo Almeida <eduardomatosalmeida@gmail.com>
2019-10-27 14:42:18 -0300

    Projeto do Curso de SpringBoot com API Rest - Implementacao de paginacao

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,17 +1,12 @@
 package br.com.alura.forum.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-	List<Topico> findByCurso_Nome(String nomeCurso);
-
-	/**
-	@Query("select * from Topico t where t.curso.nome = :nomeCurso")
-	List<Topico> carregarPorNomeCurso(@Param("nomeCurso") String nomeCurso);
-	*/
+	Page<Topico> findByCurso_Nome(String nomeCurso, Pageable paginacao);
 }
