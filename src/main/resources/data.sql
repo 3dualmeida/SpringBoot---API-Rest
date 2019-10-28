da12ac8cb9b72d0a8deff9dedee1fe2a55694835
Eduardo Almeida <eduardomatosalmeida@gmail.com>
2019-10-27 23:22:43 -0300

    Projeto do Curso de SpringBoot com API Rest - Protecao com Spring Security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$Yp6zoclTTEyN6fWHbJq8nehTJlDVCvAzFpn5LFNydT.vE6ZprfFaS');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
